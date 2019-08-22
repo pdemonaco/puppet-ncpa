86a16a0fcb81b7cef9ed10cd32a4cf28e46bd3c2
Phil DeMonaco <pdemon@gmail.com>
2019-08-22 17:49:39 -0400

    mant: conversion to PDK structure
    This makes testing much easier. Still need to get travis going, but it's
    better than nothing.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
+
 describe 'ncpa' do
   context 'with default values for all parameters' do
-    it { should contain_class('ncpa') }
+    it 'compiles with all dependencies' do
+      is_expected.to compile.with_all_deps
+    end
+
+    it 'contains the NCPA class' do
+      is_expected.to contain_class('ncpa')
+    end
   end
 end
